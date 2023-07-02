a9aac15ef6d961a8a18d0678c6309a4bfbebce76
shellRaining <shellRaining@gmail.com>
2023-07-02 17:05:36 +0800

    finish

--- lua/nvim_md_HFfont/init.lua
+++ lua/nvim_md_HFfont/init.lua
@@ -1,18 +1,70 @@
-local ts_utils = require("nvim-treesitter.ts_utils")
-
 local M = {}
 
+local api = vim.api
+
+-- 查询文本中所有半角标点的位置
+local function findPunctuationPositions()
+    local bufnr = api.nvim_get_current_buf()
+    local lines = api.nvim_buf_get_lines(bufnr, 0, -1, false)
+    local positions = {}
+
+    for i, line in ipairs(lines) do
+        local linePositions = {}
+        for position in line:gmatch("()[.,]") do
+            table.insert(linePositions, position)
+        end
+        if #linePositions > 0 then
+            positions[i] = linePositions
+        end
+    end
+
+    return positions
+end
+
+local inCodeBlock = function(node)
+    local parent = node:parent()
+
+    while parent do
+        vim.notify(parent:type())
+        if parent:type() == "fenced_code_block" then
+            return true
+        end
+        parent = parent:parent()
+    end
+
+    return false
+end
+
 M.setup = function()
-    vim.notify('hello')
-    vim.api.nvim_create_augroup("HFfont_augroup", { clear = true })
-    vim.api.nvim_create_autocmd("CursorMoved", {
-        group = "HFfont_augroup",
-        pattern = "*",
-        callback = function()
-            local node = ts_utils.get_node_at_cursor()
-            vim.notify(node:type())
-        end,
-    })
+    api.nvim_create_user_command("SubstituteHalf", function()
+        local punctuationPositions = findPunctuationPositions()
+        vim.notify(vim.inspect(punctuationPositions))
+        if #punctuationPositions == 0 then
+            print("No half font found.")
+            return
+        end
+
+        -- get the treesitter node from position
+        local cur_buf = api.nvim_get_current_buf()
+        for i, linePositions in pairs(punctuationPositions) do
+            for _, col in pairs(linePositions) do
+                local node = vim.treesitter.get_node({
+                    bufnr = cur_buf,
+                    pos = { i - 1, col - 1 },
+                })
+
+                if node and not inCodeBlock(node) then
+                    -- get the char in the position
+                    local char = api.nvim_buf_get_lines(cur_buf, i - 1, i, false)[1]:sub(col, col)
+                    if char == "." then
+                        api.nvim_buf_set_text(cur_buf, i - 1, col - 1, i - 1, col, { "。" })
+                    elseif char == "," then
+                        api.nvim_buf_set_text(cur_buf, i - 1, col - 1, i - 1, col, { "，" })
+                    end
+                end
+            end
+        end
+    end, {})
 end
 
 return M
