77df759ac37226c52e38446d6e5fc1408280ec36
shellRaining <shellRaining@gmail.com>
2023-10-16 09:32:59 +0800

    ref: more fast~ more elegant~

--- lua/nvim_md_HFfont/init.lua
+++ lua/nvim_md_HFfont/init.lua
@@ -1,113 +1,111 @@
 local M = {}
 
+---@class Position
+---@field row number
+---@field col number
+
 local api = vim.api
+local ts = vim.treesitter
 
--- 查询文本中所有半角标点的位置
-local function findPunctuationPositions()
-    local bufnr = api.nvim_get_current_buf()
-    local lines = api.nvim_buf_get_lines(bufnr, 0, -1, false)
-    local positions = {}
+-- a map to convert half-width punctuation to kanji punctuation
+local half2fullMap = {
+    [","] = "，",
+    ["."] = "。",
+}
 
-    for i, line in ipairs(lines) do
-        local linePositions = {}
-        for position in line:gmatch("()[.,]") do
-            table.insert(linePositions, position)
+local ignore = {
+    html_block = true,
+    code_block = true,
+    fenced_code_block = true,
+    atx_heading = true,
+    list_marker_dot = true,
+    inline_link = true,
+    link_destination = true,
+    code_span = true,
+    list_item = true,
+    minus_metadata = true,
+}
+
+---@param pos Position
+---@return boolean isSuit true if the position should be substituted
+local function suit(pos)
+    local res = true
+
+    local node = ts.get_parser(0, "markdown_inline"):named_node_for_range({ pos.row, pos.col, pos.row, pos.col })
+    if not node then
+        return false
+    end
+    local min_node = node:descendant_for_range(pos.row, pos.col, pos.row, pos.col)
+    while min_node do
+        if ignore[min_node:type()] then
+            res = false
+            break
         end
-        if #linePositions > 0 then
-            positions[i] = linePositions
-        end
+        min_node = min_node:parent()
     end
 
-    return positions
-end
-
-local inCodeBlock = function(node)
-    local ignoreType = {
-        "html_block",
-        "code_block",
-        "fenced_code_block",
-        "atx_heading",
-        "atx_h1_marker",
-        "atx_h2_marker",
-        "atx_h3_marker",
-        "atx_h4_marker",
-        "atx_h5_marker",
-        "atx_h6_marker",
-        "list_marker_dot",
-        "link_reference_definition",
-        "link_title",
-        "link_label",
-        "link_destination",
-    }
+    node = ts.get_node({ pos = { pos.row, pos.col - 1 } })
+    if not node then
+        return false
+    end
     while node do
-        if vim.tbl_contains(ignoreType, node:type()) then
-            return true
+        if ignore[node:type()] then
+            res = false
+            break
         end
         node = node:parent()
     end
 
-    return false
+    return res
 end
 
-local function hasLinkPattern(str)
-    local linkPattern = "%[.-]%(.-%)"
-    local m = str:find(linkPattern)
-    if m then
-        return true
-    else
-        return false
-    end
-end
+---@param startPos Position
+---@return Position | nil pos
+local function findPunctuationPosition(startPos)
+    local bufnr = api.nvim_get_current_buf()
+    local lines = api.nvim_buf_get_lines(bufnr, startPos.row, -1, false)
 
-local function execute()
-    local punctuationPositions = findPunctuationPositions()
-    -- note the key is number, so can not use # to get len
-    local count = 0
-    for _ in pairs(punctuationPositions) do
-        count = count + 1
-    end
-    if count == 0 then
-        return
-    end
-
-    -- get the treesitter node from position
-    local cur_buf = api.nvim_get_current_buf()
-    for row, linePositions in pairs(punctuationPositions) do
-        local curline = api.nvim_buf_get_lines(cur_buf, row - 1, row, false)[1]
-        if hasLinkPattern(curline) then
-            goto continue
-        end
-        for _, col in pairs(linePositions) do
-            local node = vim.treesitter.get_node({
-                bufnr = cur_buf,
-                pos = { row - 1, col - 1 },
-            })
-
-            if node and not inCodeBlock(node) then
-                -- get the char in the position
-                local char = api.nvim_buf_get_lines(cur_buf, row - 1, row, false)[1]:sub(col, col)
-                if char == "." then
-                    api.nvim_buf_set_text(cur_buf, row - 1, col - 1, row - 1, col, { "。" })
-                elseif char == "," then
-                    api.nvim_buf_set_text(cur_buf, row - 1, col - 1, row - 1, col, { "，" })
-                end
-                execute()
+    for i, line in ipairs(lines) do
+        local initPos = i == 0 and startPos.col + 1 or 0
+        local col = line:find("()[.,]", initPos)
+        if col then
+            local pos = { row = startPos.row + i - 1, col = col - 1 }
+            if suit(pos) then
+                return pos
             end
         end
-        ::continue::
     end
+
+    return nil
+end
+
+-- substitute half-width punctuation to full-width for all lines
+local function substituteHalf()
+    ---@param startPos Position
+    local function inner(startPos)
+        local pos = findPunctuationPosition(startPos)
+        if not pos then
+            return
+        end
+
+        local bufnr = api.nvim_get_current_buf()
+        local line = api.nvim_buf_get_lines(bufnr, pos.row, pos.row + 1, false)[1]
+        local char = line:sub(pos.col + 1, pos.col + 1)
+        local fullChar = half2fullMap[char]
+        if not fullChar then
+            return
+        end
+
+        api.nvim_buf_set_text(bufnr, pos.row, pos.col, pos.row, pos.col + 1, { fullChar })
+
+        inner({ row = pos.row, col = pos.col + 2 })
+    end
+
+    inner({ row = 0, col = 0 })
 end
 
 M.setup = function()
-    api.nvim_create_user_command("SubstituteHalf", execute, {})
-    -- api.nvim_create_autocmd("CursorMoved", {
-    --     pattern = "*.md",
-    --     callback = function()
-    --         local node = vim.treesitter.get_node()
-    --         local node_type = node:type()
-    --         vim.notify(node_type)
-    --     end,
-    -- })
+    api.nvim_create_user_command("SubstituteHalf", substituteHalf, {})
 end
 
 return M
